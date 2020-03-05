99e00832efdeb31c589f1fe4a952fdb3539be2d4
Raghavan SS <rsampige@asu.edu>
2020-03-04 19:46:26 -0700

    Checking double click on icon class

--- src/Icons.java
+++ src/Icons.java
@@ -1,11 +1,17 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JOptionPane;
 
 /**
  *  @author Raghavan
  *  @version 1.0
  */
-public abstract class Icons {
+public abstract class Icons implements MouseListener {
+	
+	public String description;
 	
 	public abstract void drawShape(Graphics graphic);
 	
@@ -15,5 +21,12 @@ public abstract class Icons {
 	
 	public abstract void setLocation(Point point);
 	
+	public void mouseClicked(MouseEvent event) {
+		if(event.getClickCount() == 2)
+			openDialogue();
+	}
 	
+	public void openDialogue() {
+		String s = (String)JOptionPane.showInputDialog("Enter description");
+	}
 }
