3c87d77ccd1e2be54fb95c9c86482c861fd2fee9
Raghavan SS <rsampige@asu.edu>
2020-03-04 19:29:18 -0700

    Changing option names

--- src/OptionNames.java
+++ src/OptionNames.java
@@ -1,8 +1,16 @@
 
 public class OptionNames {
-	public final static String TRIANGLE = "TRIANGLE";
+	public final static String OPENBRACKET = "OPEN_BRACKET";
 	
-	public final static String SQUARE = "SQUARE";
+	public final static String CLOSEBRACKET = "CLOSE_BRACKET";
 	
-	public final static String CIRCLE = "CIRCLE";
+	public final static String LESSTHAN = "LESS_THAN";
+	
+	public final static String GREATERTHAN = "GREATER_THAN";
+
+	public final static String ATTHERATE = "AT_THE_RATE";
+	
+	public final static String HYPHEN = "HYPHEN";
+	
+	public final static String BARS = "BARS";
 }
