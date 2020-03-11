75f42c50d55a6530ea4b4aa9316512b28f5fd1ac
Raghavan SS <rsampige@asu.edu>
2020-03-11 14:06:14 -0700

    Converted dot to jbutton in LessThan class

--- src/View/LessThan.java
+++ src/View/LessThan.java
@@ -12,26 +12,27 @@ public class LessThan extends Icons {
 	private Dot output1;
 	private Dot output2;
 	private Shape icon;
+	private Point inputPoint, outputPoint1, outputPoint2;
 	
 	public LessThan(Point point) {
 		this.point = point;
+		inputPoint = new Point((int)point.getX() + 10, (int)point.getY()+ 20);
+		outputPoint1 = new Point((int)point.getX() + 80, (int)point.getY()+ 10);
+		outputPoint2 = new Point((int)point.getX() + 80, (int)point.getY()+ 30);
+		this.input = new Dot(inputPoint, true);
+		this.output1 = new Dot(outputPoint1, true);
+		this.output2 = new Dot(outputPoint2, false);
 	}
 
 	@Override
 	public void drawShape(Graphics graphic) {
 		Graphics2D graphics2 = (Graphics2D) graphic;
-		Point inputPoint = new Point();
 		inputPoint.setLocation(point.getX() + 10, point.getY() + 20);
-		this.input = new Dot(inputPoint, true);
-		this.input.drawShape(graphic);
-		Point outputPoint1 = new Point();
 		outputPoint1.setLocation(point.getX() + 80, point.getY() + 10);
-		this.output1 = new Dot(outputPoint1, false);
-		this.output1.drawShape(graphic);
-		Point outputPoint2 = new Point();
 		outputPoint2.setLocation(point.getX() + 80, point.getY() + 30);
-		this.output2 = new Dot(outputPoint2, false);
-		this.output2.drawShape(graphic);
+		input.drawShape();
+		output1.drawShape();
+		output2.drawShape();
 		graphics2.setFont(new Font("Monospaced", Font.BOLD, 32));
 		graphics2.drawString("<", (int)point.getX() + 40, (int)point.getY() + 35);
 		icon = new Rectangle2D.Double(this.point.getX(), this.point.getY(), 100, 50);
